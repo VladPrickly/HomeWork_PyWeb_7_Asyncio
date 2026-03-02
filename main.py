7590a1b9b39a372f8a811a7455d89ed0a9c36f6c
Vladislav <vladislav.goltsev@gmail.com>
2026-03-03 00:02:48 +0300

    modify get_info and main

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import requests
 import asyncio, aiohttp
+from itertools import batched
 
-async def get_info(person_id: int):
-    session = aiohttp.ClientSession()
+MAX_REQUESTS = 5
+
+async def get_info(person_id: int, session: aiohttp.ClientSession):
     http_response = await session.get(f"https://www.swapi.tech/api/people/{person_id}/")
     json_data = await http_response.json()
-    await session.close()
     return json_data
 
 
 async def main():
-    coro_1 = get_info(1)
-    coro_2 = get_info(2)
+    async with aiohttp.ClientSession() as session:
+        for id_b in batched(range(1, 101), MAX_REQUESTS):
+            coros = []
+            for i in id_b:
+                coro = get_info(i, session)
+                coros.append(coro)
+            response = await asyncio.gather(*coros)
 
-    response = await asyncio.gather(coro_1, coro_2)
-
-    print(response)
+            print(response)
 
 asyncio.run(main())
